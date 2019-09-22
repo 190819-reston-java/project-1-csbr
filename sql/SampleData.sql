44ba5743f2bec1f2ebd969c53ef0f7368eace692
Carlos Santos <csantos2@uncc.edu>
2019-09-22 02:57:49 -0400

    Finished initial database design.

---
+++ sql/SampleData.sql
@@ -0,0 +1,33 @@
+insert into work_table(work_id,employee_or_manager,first_name,last_name,acc_password,acc_email)
+values ('asdf134',true,'Engel','Sanders','1234','eee@555.com');
+
+insert into work_table(work_id,employee_or_manager,first_name,last_name,acc_password,acc_email)
+values ('dfsg45',true,'Will','Fengel','zxcv','rret@ett.com');
+
+insert into work_table(work_id,employee_or_manager,first_name,last_name,acc_password,acc_email)
+values ('whois',false,'Qino','Sanchez','qwer','aaa@eee.com');
+
+
+
+insert into reimbursements(reimb_id,work_emp_id_fk,emp_fk,reimb_status,pending,
+							reimb_balance,work_mgr_id_fk,mgr_fk) values
+		('235gfg','asdf134',true,false,true,40.00,'whois',false);
+insert into reimbursements(reimb_id,work_emp_id_fk,emp_fk,reimb_status,pending,
+							reimb_balance,work_mgr_id_fk,mgr_fk) values
+		('234gfg','asdf134',true,false,true,20.00,'whois',false);
+
+select * from work_table;
+select * from reimbursements;
+select * from reimb_reciepts;
+
+insert into reimb_reciepts(reimb_id_fk,reciept_img_path) 
+values ('235gfg','/users/blah/example.jpg');
+insert into reimb_reciepts(reimb_id_fk,reciept_img_path) 
+values ('235gfg','/users/blah/another_example.jpg');
+insert into reimb_reciepts(reimb_id_fk,reciept_img_path) 
+values ('233gfg','/users/blah/another_example.jpg');
+
+delete from reimb_reciepts where reimb_id_fk = '235gfg';
+
+drop table reimb_reciepts;
+alter table 
---
+++ sql/WorkerTable.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE ReimbursementProgram;
+
+CREATE TABLE work_table (
+
+	work_id VARCHAR(20) UNIQUE, -- Worker's username
+	employee_or_manager BOOLEAN NOT NULL, -- Employee: True, Manager: False
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	acc_password varchar(50) NOT NULL,
+	acc_email VARCHAR(40) NOT NULL,
+	PRIMARY KEY (work_id, employee_or_manager)
+);
+
+CREATE TABLE contact_info (
+
+	work_id_fk VARCHAR(15) UNIQUE,
+	eom_fk BOOLEAN NOT NULL,
+	address VARCHAR(50) NOT NULL,
+	city VARCHAR(20) NOT NULL,
+	country VARCHAR(20) NOT NULL,
+	state VARCHAR(15) NULL,
+	zipcode VARCHAR(5) NULL,
+
+	FOREIGN KEY (work_id_fk, eom_fk) REFERENCES 
+	work_table(work_id, employee_or_manager)
+
+);
+
+CREATE TABLE reimbursements (
+
+	reimb_id varchar(8) PRIMARY KEY,
+	work_emp_id_fk VARCHAR(15) NOT NULL,
+	emp_fk BOOLEAN CHECK(emp_fk = true),
+	reimb_status BOOLEAN NOT NULL,
+	pending BOOLEAN,
+	reimb_balance NUMERIC CHECK(reimb_balance >= 0.0),
+	work_mgr_id_fk VARCHAR(15) NULL,
+	mgr_fk BOOLEAN CHECK(mgr_fk = false),
+
+	FOREIGN KEY (work_emp_id_fk, emp_fk) REFERENCES 
+	work_table(work_id, employee_or_manager),
+	FOREIGN KEY (work_mgr_id_fk, mgr_fk) REFERENCES 
+	work_table(work_id, employee_or_manager)
+	
+);
+
+CREATE TABLE reimb_reciepts (
+
+	reimb_id_fk varchar(8) NOT NULL,
+	reciept_img_path VARCHAR(100) NOT NULL,
+	FOREIGN KEY (reimb_id_fk) REFERENCES reimbursements(reimb_id)
+
+);
