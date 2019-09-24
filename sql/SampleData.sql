919c2c4a6692f8993d461194da6eb428d1b066cb
Carlos Santos <csantos2@uncc.edu>
2019-09-24 13:33:31 -0400

    Redesigned database, again. This time, the contacts table is left out.

--- sql/SampleData.sql
+++ sql/SampleData.sql
@@ -7,32 +7,21 @@ values ('dfsg45',true,'Will','Fengel','zxcv','rret@ett.com');
 insert into work_table(work_id,employee_or_manager,first_name,last_name,acc_password,acc_email)
 values ('whois',false,'Qino','Sanchez','qwer','aaa@eee.com');
 
-
-
-insert into reimbursements(reimb_id,work_emp_id_fk,emp_fk,reimb_status,pending,
-							reimb_balance,work_mgr_id_fk,mgr_fk) values
-		('235gfg','asdf134',true,false,true,40.00,'whois',false);
-insert into reimbursements(reimb_id,work_emp_id_fk,emp_fk,reimb_status,pending,
-							reimb_balance,work_mgr_id_fk,mgr_fk) values
-		('234gfg','asdf134',true,false,true,20.00,'whois',false);
+--drop table reimb_employee;
+--drop table reimbursements;
+--drop table contact_addresses;
+--drop table work_table;
 
 select * from work_table;
 select * from reimbursements;
-select * from reimb_reciepts;
+select * from reimb_employee;
 
+insert into reimb_manager(reimb_id_fk,reimb_status,
+			reimb_balance,work_mgr_id_fk,mgr_fk)
+		values('342rf','PENDING',20.00,'whois',false);
+insert into reimb_employee(reimb_id,work_emp_id_fk,emp_fk,reciept_img_path)
+	values('342rf','dfsg45',true,'c:/blah/eee.jpg');
 
-UPDATE reimbursements SET reimb_status = false WHERE work_mgr_id_fk = 'whois';
+UPDATE reimb_manager SET reimb_status = false WHERE work_mgr_id_fk = 'whois';
 update work_table set employee_or_manager = false where work_id = 'asdf134';
 
-insert into reimb_reciepts(reimb_id_fk,reciept_img_path) 
-values ('235gfg','/users/blah/example.jpg');
-insert into reimb_reciepts(reimb_id_fk,reciept_img_path) 
-values ('235gfg','/users/blah/another_example.jpg');
--- this query doesn't work since there's no '233gfg' in the table.
-insert into reimb_reciepts(reimb_id_fk,reciept_img_path) 
-values ('233gfg','/users/blah/another_example.jpg');
-
-delete from reimb_reciepts where reimb_id_fk = '235gfg';
-
-drop table reimb_reciepts;
-alter table reimb_reciepts 
--- sql/WorkerTable.sql
+++ sql/WorkerTable.sql
@@ -8,47 +8,48 @@ CREATE TABLE work_table (
 	last_name VARCHAR(20) NOT NULL,
 	acc_password varchar(50) NOT NULL,
 	acc_email VARCHAR(40) NOT NULL,
+	address VARCHAR(50) NULL,
+	city VARCHAR(20) NULL,
+	country VARCHAR(20) NULL,
 	PRIMARY KEY (work_id, employee_or_manager)
 );
 
-CREATE TABLE contact_addresses (
+--CREATE TABLE contact_addresses (
+--
+--	work_id_fk VARCHAR(15) UNIQUE,
+--	address VARCHAR(50) NOT NULL,
+--	city VARCHAR(20) NOT NULL,
+--	country VARCHAR(20) NOT NULL,
+--	state VARCHAR(15) NULL,
+--	zipcode VARCHAR(5) NULL,
+--
+--	FOREIGN KEY (work_id_fk) REFERENCES 
+--	work_table(work_id)
+--
+--);
 
-	work_id_fk VARCHAR(15) UNIQUE,
-	address VARCHAR(50) NOT NULL,
-	city VARCHAR(20) NOT NULL,
-	country VARCHAR(20) NOT NULL,
-	state VARCHAR(15) NULL,
-	zipcode VARCHAR(5) NULL,
+CREATE TABLE reimb_manager (
 
-	FOREIGN KEY (work_id_fk) REFERENCES 
-	work_table(work_id)
-
-);
-
-CREATE TABLE reimbursements (
-
-	reimb_id varchar(8) PRIMARY KEY,
-	reimb_status BOOLEAN NOT NULL,
-	pending BOOLEAN,
+	reimb_id_fk varchar(8) NOT NULL,
+	reimb_status varchar(10) not null,
 	reimb_balance NUMERIC CHECK(reimb_balance >= 0.0),
 	work_mgr_id_fk VARCHAR(15) NULL,
 	mgr_fk BOOLEAN CHECK(mgr_fk = false),
 
-	FOREIGN KEY (work_emp_id_fk, emp_fk) REFERENCES 
-	work_table(work_id, employee_or_manager),
 	FOREIGN KEY (work_mgr_id_fk, mgr_fk) REFERENCES 
-	work_table(work_id, employee_or_manager)
+	work_table(work_id, employee_or_manager),
+	FOREIGN KEY (reimb_id_fk) REFERENCES reimb_employee(reimb_id)
 	
 );
 
 CREATE TABLE reimb_employee (
 
-	reimb_id_fk varchar(8) NOT NULL,
+	reimb_id varchar(8) PRIMARY KEY,
 	work_emp_id_fk VARCHAR(15) NOT NULL,
 	emp_fk BOOLEAN CHECK(emp_fk = true),
 	reciept_img_path VARCHAR(100) NOT NULL,
 	FOREIGN KEY (work_emp_id_fk, emp_fk) REFERENCES 
-	work_table(work_id, employee_or_manager),
-	FOREIGN KEY (reimb_id_fk) REFERENCES reimbursements(reimb_id)
+	work_table(work_id, employee_or_manager)
 
 );
+
